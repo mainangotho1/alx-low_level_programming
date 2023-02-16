bad6b407070674ff78e43bdaea830e3b3b10895b
mainangotho1 <mainangotho1@gmail.com>
2023-02-16 15:23:19 -0800

    aga

--- 0x00-hello_world/5-prinf.c
+++ 0x00-hello_world/5-prinf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>                                                                  
 /**                                                                                 
- *  * main - Prints out line of code.                            
- *   * Return: 0 if it is successful.                                                        
+ * main - Prints out line of code.                            
+ * Return: 0 if it is successful.                                                        
 */                                                                                  
 int main(void)                                                                      
 {                                                                                   
 printf("with proper grammar, but the outcome is a piece of art,\n");                
 return (0);
+}
