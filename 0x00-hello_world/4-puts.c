6c8698811d715f1cb0a9bf42b1af3057a91a9b97
mainangotho1 <mainangotho1@gmail.com>
2023-02-16 15:19:40 -0800

    task number 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- * main -  print a line of code using puts
+/**
+ *  main- print a line of code
  *
- * return 0
-*/
+ *  Return 0
+ */
 int main(void)
-{    puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>                                                                  
+/**                                                                                 
+ *  * main - Prints out line of code.                            
+ *   * Return: 0 if it is successful.                                                        
+*/                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("with proper grammar, but the outcome is a piece of art,\n");                
+return (0);
